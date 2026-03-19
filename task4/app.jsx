26d738a6c6146ad449db27f73aa973a248acf59c
bhargavvvk <apple@Apples-MacBook-Air.local>
2026-03-19 18:03:35 +0530

    changed app.jsx

--- task4/app.jsx
+++ task4/app.jsx
@@ -9,7 +9,7 @@ function App() {
         <Router>
             <Switch>            
                 <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
+                <Route path="/aboutme" component={Aboutme} />
                 <Route path="/contact" component={Contact} />
             </Switch>
         </Router>
